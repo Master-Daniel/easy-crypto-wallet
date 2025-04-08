3737ab1b3ed6e6c7f8d6afa229c8eaf3a79afaf3
Master-Daniel <engrdanywiss@gmail.com>
2025-04-08 16:17:10 +0100

    created market module

---
+++ src/coin-market/coin-market.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { CoinMarketService } from './coin-market.service';
+import { AdminSettingsService } from '../admin/admin-settings.service';
+import { CoinMarketController } from './coin-market.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { AdminModule } from '../admin/admin.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { AdminSettings } from '../admin/entities/admin-settings.entity';
+
+@Module({
+  imports: [
+    AdminModule,
+    MikroOrmModule.forFeature([AdminSettings]),
+    CacheModule.register(),
+  ],
+  controllers: [CoinMarketController],
+  providers: [CoinMarketService, AdminSettingsService],
+  exports: [CoinMarketService],
+})
+export class CoinMarketModule {}
