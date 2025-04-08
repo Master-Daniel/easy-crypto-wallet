e9da9dd94a4b3418a95caca450ac93ede8e9dece
Master-Daniel <engrdanywiss@gmail.com>
2025-04-08 15:59:24 +0100

    updated with controllers, cache config, rate limiter and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,38 @@ import { UserModule } from './user/user.module';
 import { WalletModule } from './wallet/wallet.module';
 import { AuthModule } from './auth/auth.module';
 import mikroOrmConfig from './mikro-orm.config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CoinMarketController } from './coin-market/coin-market.controller';
+import { AdminSettingsController } from './admin/admin-settings.controller';
+import { CoinMarketModule } from './coin-market/coin-market.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { AdminModule } from './admin/admin.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(mikroOrmConfig),
+    ScheduleModule.forRoot(),
+    CacheModule.register({
+      ttl: 60,
+      max: 100,
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 10,
+        },
+      ],
+    }),
     UserModule,
     WalletModule,
     AuthModule,
+    CoinMarketModule,
+    AdminModule,
   ],
-  controllers: [],
+  controllers: [CoinMarketController, AdminSettingsController],
   providers: [],
 })
 export class AppModule {}
