1d557ff07f5ee89a9e060706d1435955fa1c3376
Master-Daniel <engrdanywiss@gmail.com>
2025-04-08 16:18:30 +0100

    created seeder for admin settings

---
+++ src/seeders/admin-settings.seeder.ts
@@ -0,0 +1,26 @@
+import { Seeder } from '@mikro-orm/seeder';
+import { AdminSettings } from '../admin/entities/admin-settings.entity';
+import { EntityManager } from '@mikro-orm/mysql';
+
+export class AdminSettingsSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const defaults = [
+      { key: 'maintenance_mode', value: 'false' },
+      { key: 'default_currency', value: 'USD' },
+      { key: 'kyc_required', value: 'true' },
+      { key: 'coin_list', value: 'bitcoin,ethereum,dogecoin' },
+      { key: 'coin_trade_list', value: 'bitcoin,ethereum' },
+      { key: 'elite_coin_key', value: 'abc123' },
+      { key: 'elite_coin_domain', value: 'http://localhost:2000' },
+    ];
+
+    for (const setting of defaults) {
+      const exists = await em.findOne(AdminSettings, { key: setting.key });
+      if (!exists) {
+        em.create(AdminSettings, setting);
+      }
+    }
+
+    await em.flush();
+  }
+}
