a15c196c919c461b8f487ef58a9b18dbb9f2669c
Master-Daniel <engrdanywiss@gmail.com>
2025-04-13 21:04:49 +0100

    removed the cache module since it has been registered globally in the app.module

--- src/coin-market/coin-market.module.ts
+++ src/coin-market/coin-market.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { CoinMarketService } from './coin-market.service';
 import { AdminSettingsService } from '../admin/admin-settings.service';
 import { CoinMarketController } from './coin-market.controller';
-import { CacheModule } from '@nestjs/cache-manager';
 import { AdminModule } from '../admin/admin.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AdminSettings } from '../admin/entities/admin-settings.entity';
 
 @Module({
-  imports: [
-    AdminModule,
-    MikroOrmModule.forFeature([AdminSettings]),
-    CacheModule.register(),
-  ],
+  imports: [AdminModule, MikroOrmModule.forFeature([AdminSettings])],
   controllers: [CoinMarketController],
   providers: [CoinMarketService, AdminSettingsService],
   exports: [CoinMarketService],
