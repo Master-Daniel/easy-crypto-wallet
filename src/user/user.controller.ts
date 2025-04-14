dca5e08fe0f08cf2afb627f6f3db38c3f7bd9cd5
Master-Daniel <engrdanywiss@gmail.com>
2025-04-14 16:47:38 +0100

    added new routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,6 +12,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { OtpDto } from './dto/otp-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import { ResendOtpDto } from './dto/resend-otp.dto';
 
 @Controller('users')
 export class UserController {
@@ -26,12 +29,46 @@ export class UserController {
     return await this.userService.create(createUserDto);
   }
 
+  @Post('/login')
+  async login(@Body() loginDto: LoginUserDto): Promise<{
+    message: string;
+    status: number;
+    token: string;
+    data: ReturnType<User['toJSON']>;
+  }> {
+    return await this.userService.login(loginDto);
+  }
+
   @Post('/verify-otp')
   async verifyOtp(@Body() otpDto: OtpDto): Promise<{
     message: string;
     status: number;
   }> {
     return await this.userService.verifyOtp(otpDto);
+  }
+
+  @Post('/resend-otp')
+  async resendOtp(@Body() resendOtpDto: ResendOtpDto): Promise<{
+    message: string;
+    status: number;
+  }> {
+    return await this.userService.resendOtp(resendOtpDto);
+  }
+
+  @Post('/forgot-password')
+  async forgotPassword(@Body() resendOtpDto: ResendOtpDto): Promise<{
+    message: string;
+    status: number;
+  }> {
+    return await this.userService.forgotPassword(resendOtpDto);
+  }
+
+  @Post('/reset-password')
+  async resetPassword(@Body() resetPasswordDto: ResetPasswordDto): Promise<{
+    message: string;
+    status: number;
+  }> {
+    return await this.userService.resetPassword(resetPasswordDto);
   }
 
   @Get()
