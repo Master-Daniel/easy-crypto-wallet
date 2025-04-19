fb4b121003000554f622c00be0d710ef979e9962
Master-Daniel <engrdanywiss@gmail.com>
2025-04-19 05:20:18 +0100

    added tier seeder

--- src/seeders/database.seeder.ts
+++ src/seeders/database.seeder.ts
@@ -2,9 +2,10 @@ import { EntityManager } from '@mikro-orm/mysql';
 import { Seeder } from '@mikro-orm/seeder';
 import { AdminSeeder } from './admin.seeder';
 import { AdminSettingsSeeder } from './admin-settings.seeder';
+import { TierSeeder } from './TierSeeder';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    return this.call(em, [AdminSeeder, AdminSettingsSeeder]);
+    return this.call(em, [AdminSeeder, AdminSettingsSeeder, TierSeeder]);
   }
 }
