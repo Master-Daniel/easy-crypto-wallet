2f18f29705f496baeb83ce9dc93fdba547b68b13
Master-Daniel <engrdanywiss@gmail.com>
2025-04-22 14:04:07 +0100

    created transfer route

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { WalletService } from './wallet.service';
+import { WalletTransferDto } from './dto/wallet-transfer.dto';
 
 @Controller('wallet')
-export class WalletController {}
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Post('/transfer')
+  async transfer(
+    @Body(new ValidationPipe()) walletTransferDto: WalletTransferDto,
+  ): Promise<{
+    message: string;
+    status: number;
+  }> {
+    return await this.walletService.transfer(walletTransferDto);
+  }
+}
