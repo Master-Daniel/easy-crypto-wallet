6d10d81f54f88742c6f25fc2dabc5c6640ff97df
Master-Daniel <engrdanywiss@gmail.com>
2025-05-02 07:08:28 +0100

    Adds CRUD endpoints for Tier management
    Introduces create, update, and delete endpoints for Tier entities.
    Enhances existing endpoints with validation and improved response structure.
    
    Improves code maintainability and consistency.

--- src/tier/tier.controller.ts
+++ src/tier/tier.controller.ts
@@ -1,17 +1,56 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TierService } from './tier.service';
+import { TierDto } from './dto/tier.dto';
+import { Tier } from './entity/tier.entity';
 
 @Controller('tier')
 export class TierController {
   constructor(private readonly tierService: TierService) {}
 
+  @Post('/create')
+  create(
+    @Body(new ValidationPipe()) tierDto: TierDto,
+  ): Promise<{ message: string; status: number }> {
+    return this.tierService.create(tierDto);
+  }
+
   @Get()
-  findAll() {
+  findAll(): Promise<{
+    message: string;
+    status: number;
+    data: Tier[];
+  }> {
     return this.tierService.findAll();
   }
 
   @Get('/:id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<{
+    message: string;
+    status: number;
+    data: Tier;
+  }> {
     return this.tierService.findOne(id);
+  }
+
+  @Patch('update/:id')
+  async updateTier(
+    @Param('id') id: string,
+    @Body() updateTierDto: Partial<TierDto>,
+  ) {
+    return this.tierService.patch(id, updateTierDto);
+  }
+
+  @Delete('delete/:id')
+  async deleteTier(@Param('id') id: string) {
+    return this.tierService.delete(id);
   }
 }
