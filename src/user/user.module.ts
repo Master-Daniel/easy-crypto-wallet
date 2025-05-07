def2908202e06eba2d5060a6ca0c545485b21c00
Master-Daniel <engrdanywiss@gmail.com>
2025-05-07 06:09:01 +0100

    Enhances user module with wallet and transaction support
    Adds forward references to WalletModule and TransactionModule to handle circular dependencies. Expands supported entities with Transaction and services with WalletService, TransactionService, and NotificationService. Updates exports to include MikroOrmModule for broader usability.
    
    Improves modularity and feature integration.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,5 @@
 import {
+  forwardRef,
   MiddlewareConsumer,
   Module,
   NestModule,
@@ -14,11 +15,28 @@ import { AuthMiddleware } from '../middleware/auth.middleware';
 import { AdminAuthMiddleware } from '../middleware/admin-auth';
 import { Admin } from '../admin/entities/admin.entity';
 import { AdminService } from '../admin/admin.service';
+import { WalletService } from '../wallet/wallet.service';
+import { TransactionService } from '../transaction/transaction.service';
+import { TransactionModule } from '../transaction/transaction.module';
+import { Transaction } from '../transaction/entity/transaction.entity';
+import { WalletModule } from '../wallet/wallet.module';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User, Wallet, Admin])],
+  imports: [
+    MikroOrmModule.forFeature([User, Wallet, Admin, Transaction]),
+    forwardRef(() => TransactionModule),
+    forwardRef(() => WalletModule),
+  ],
   controllers: [UserController],
-  providers: [UserService, MailService, AdminService],
-  exports: [UserService],
+  providers: [
+    UserService,
+    MailService,
+    AdminService,
+    WalletService,
+    TransactionService,
+    NotificationService,
+  ],
+  exports: [UserService, MikroOrmModule],
 })
 export class UserModule {}
