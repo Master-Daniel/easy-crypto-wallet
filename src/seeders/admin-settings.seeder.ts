c0dfd85aa87e98210b1c0ae5a0257ee0ec355a4f
Master Daniel <engrdanywiss@gmail.com>
2025-05-17 17:16:18 +0100

    Adds bonus percentage setting to admin seeder
    Introduces a new configuration key 'bonus_percentage' in the admin
    settings seeder to allow flexible control over bonus allocation. This
    enhancement supports easier adjustments of bonus rates in the system
    configuration.

--- src/seeders/admin-settings.seeder.ts
+++ src/seeders/admin-settings.seeder.ts
@@ -11,6 +11,7 @@ export class AdminSettingsSeeder extends Seeder {
       { key: 'coin_list', value: 'bitcoin,ethereum,dogecoin' },
       { key: 'coin_trade_list', value: 'bitcoin,ethereum' },
       { key: 'elite_coin_key', value: 'abc123' },
+      { key: 'bonus_percentage', value: '1' },
       { key: 'admin_email', value: 'fxtfxt25@gmail.com' },
       { key: 'admin_alt_email', value: 'vitctorbitten@gmail.com' },
       { key: 'elite_coin_domain', value: 'http://localhost:2000' },
