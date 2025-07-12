12ae04ed8c4a8598fbcefb8e0fd6ea6ad24a6cc8
Master Daniel <engrdanywiss@gmail.com>
2025-07-12 09:30:26 +0100

    fix: removed unused methods

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,18 +1,10 @@
-import {
-  forwardRef,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { User } from './entities/user.entity';
 import { Wallet } from '../wallet/entities/wallet.entity';
 import { MailService } from '../utils/send-mail.util';
-import { AuthMiddleware } from '../middleware/auth.middleware';
-import { AdminAuthMiddleware } from '../middleware/admin-auth';
 import { Admin } from '../admin/entities/admin.entity';
 import { AdminService } from '../admin/admin.service';
 import { WalletService } from '../wallet/wallet.service';
